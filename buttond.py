e02c1d301eb9914308d917150b432a4e386cb9fb
Phillip Sitbon <phillip.sitbon@intel.com>
2014-09-08 15:25:39 -0700

    Add button timing support for short and long press.
    Signed-off-by: Phillip Sitbon <phillip.sitbon@intel.com>

--- buttond.py
+++ buttond.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python 
 import sys, os
-from time import sleep
+from time import time, sleep
 from subprocess import Popen
 from edi2c import ads1x15
 
@@ -9,14 +9,22 @@ BUTTON_COMMAND = "python"
 BUTTON_SCRIPT = "led.py"
 BUTTON_ARGS = ["proximity_simulation"]
 
+BUTTON_LONG_PRESS_COMMAND = "python"
+BUTTON_LONG_PRESS_SCRIPT = "led.py"
+BUTTON_LONG_PRESS_ARGS = ["proximity"]
+
 PATH = os.path.dirname(__file__)
 
 script_path = os.path.join(PATH, BUTTON_SCRIPT)
 script_arg = [BUTTON_COMMAND, script_path] + BUTTON_ARGS
+long_press_script_path = os.path.join(PATH, BUTTON_LONG_PRESS_SCRIPT)
+long_press_script_arg = [BUTTON_LONG_PRESS_COMMAND, script_path] + BUTTON_LONG_PRESS_ARGS
+
 process = None
 
 adc = ads1x15.ADS1X15(ic=ads1x15.IC_ADS1115)
 down = False
+down_time = 0
 
 def main():
     global down
@@ -27,34 +35,54 @@ def main():
 
             if value > 3000:
                 if not down:
+                    down_time = time()
                     down = True
-                    run_command()
-                    sleep(0.700)
-                else:
-                    sleep(0.333)
+
+                sleep(0.250)
             else:
-                down = False
-                sleep(0.120)
-                
+                if down:
+                    down = False
+                    down_time = time() - down_time
+                    long_press = False
+
+                    print >>sys.stderr, "BUTTON:", "press time", round(down_time, 2)
+                    
+                    if down_time >= 0.200:
+                        if down_time >= 3.5:
+                            long_press = True
+
+                        run_command(long_press)
+                        sleep(0.500)
+                    elif process is not None and process.poll() is None:
+                        print >>sys.stderr, "BUTTON:", "terminating"
+                        process.terminate()
+                        sleep(0.350)
+
+                sleep(0.075)
+
         except Exception, e:
             print >>sys.stderr, "BUTTON:", str(e)
             continue
 
 
-def run_command():
+def run_command(long_press=False):
     global process
+    
+    if long_press:
+        popen_arg = long_press_script_arg
+    else:
+        popen_arg = script_arg
     
     if process is None or process.poll() is not None:
         try:
-            print >>sys.stderr, "BUTTON:", " ".join(script_arg)
-            process = Popen(script_arg)
+            print >>sys.stderr, "BUTTON:", " ".join(popen_arg)
+            process = Popen(popen_arg)
         except Exception, e:
             print >>sys.stderr, "BUTTON:", str(e)
     else:
         print >>sys.stderr, "BUTTON:", "terminating"
         process.terminate()
         process = None
-
 
 if __name__ == '__main__':
     main()
