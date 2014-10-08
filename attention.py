74ef633dfe1df7868090120b19822b9c35f424e7
Karolina Cengija <karolina.cengija@intel.com>
2014-10-08 00:55:08 -0700

    activates lights using mindwave

--- attention.py
+++ attention.py
@@ -4,11 +4,17 @@ import thinkgear
 import requests
 
 def main():
+    sp_lights = subprocess.Popen(['python', 'led.py', 'mindwave'],
+                                stdin = subprocess.PIPE,
+                                stdout = subprocess.PIPE,
+                                stderr = subprocess.PIPE, shell = False)
+
     for pkt in thinkgear.ThinkGearProtocol('/dev/rfcomm0').get_packets():
         for d in pkt:
             if isinstance(d, thinkgear.ThinkGearAttentionData):
                 print d.value
+                sp_lights.stdin.write(str(d.value) + '\n')
                 sys.stdout.flush()
-
+                
 if __name__ == '__main__':
     main()
