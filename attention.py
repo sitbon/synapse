eb2324b3f7a52cdde06c3f9a57d902a8e8361e59
Karolina Cengija <karolina.cengija@intel.com>
2014-12-15 00:48:49 -0800

    adds comments for code review

--- attention.py
+++ attention.py
@@ -6,16 +6,29 @@ import camera
 from threading import Thread
 from time import sleep
 
+# these are here because I'm not using a queue for the threads
+# should I use a queue? 
 value = 0
 last_file_copied = None
 copying_files = False
 
+""" 
+    Assumes init_bluetooth.sh already set up the bluetooth connection
+    then reads attention value and activates the lights based on it.
+"""
 def main():
+    # this is here because I was trying to be consistent with dress.py
+    # but maybe it would be better to change it to led.mindwave(d.value)
+    # initially dress.py was driving everything and attention.py was just providing 
+    # d.value to std.out 
     sp_lights = subprocess.Popen(['python', 'led.py', 'mindwave'],
                                 stdin = subprocess.PIPE,
                                 stdout = subprocess.PIPE,
                                 stderr = subprocess.PIPE, shell = False)
-
+    
+    # have to mark it global again because we're not using a queue 
+    # I'm interested to know if this is just a terrible idea and what consequences 
+    # we could have?
     global value
     Thread(target=handleCamera).start()
     for pkt in thinkgear.ThinkGearProtocol('/dev/rfcomm0').get_packets():
@@ -27,9 +40,19 @@ def main():
                 sp_lights.stdin.write(str(d.value) + '\n')
                 sys.stdout.flush()
 
+"""
+   We want to take a picture when the attention value is above 80 and then record a video for the duration 
+   that it stays above 80. The camera will lock up if it gets too many http get requests too fast and so I added
+   a 1 second sleep you'll see below. The camera also takes 1 to 2 seconds to process a "start/stop" recording call
+   so we will sometimes record when eeg value has dropped below 80. 
+"""  
 def handleCamera():
   is_recording = False
   while True:
+    # added this sleep here to give the main() thread a chance to read values from bluetooth
+    # i'm assumign this is the effect of cpython global interpreter lock? 
+    # again would using a queue and/or threading.Event object be better? 
+    # what's the advantage? does this implementation make your hair stand up?  
     sleep(1)
     if value >= 40:
       if not is_recording:
@@ -41,20 +64,30 @@ def handleCamera():
         camera.stop_recording()
         is_recording = False
       
+        # i was really lazy at this point and this is how i decided 
+        # to copy files. please make a better suggestion if this bothers you
         if not copying_files:
           Thread(target=handleCameraData).start()
 
+""" Copies files from camera to web_Server/downloads/ for now, please make a suggestion for a better location?
+    using wget with -nc option to only copy files that aren't already in downloads folder
+"""
 def handleCameraData():
   global last_file_copied
   global copying_files
   copying_files = True
 
+  # i'm using wget to copy files i know i could have used libcurl
+  # but i didn't know wat the advantage would be or the risk in doing it this way
+  # also because i'm using the -nc flag we probably don't need to track last_file_copied
+  # a new wget process gets issued for every file that is being copied and they could all be running at the same time
+  # this could be a total disaster, but I didn't see one while testing so I left it
+  # i'm open to suggestions 
   print 'copying latest picture & video to web server'
   files = camera.get_latest_files(last_file_copied)  
   if len(files) > 0:
     for file in files:
        subprocess.Popen(['wget', '-nc', file,'-P', 'web_server/downloads/'])
- 
     last_file_copied = files[-1]
  
   copying_files = False 
--- camera.py
+++ camera.py
@@ -2,8 +2,6 @@ import requests
 import re
 
 IP = '192.168.42.20'
-GET_VIDEO_FILE = 'fd_get_latest_media_file_2&type=0'
-GET_PHOTO_FILE = 'fd_get_latest_media_file&type=1'
 LIST_ALL_FILES = 'fd_list_files&0'
 LIST_NEW_FILES = 'fd_list_files&referenceFile='
 PHOTO_MODE = 'fd_set_capture_mode&data=1' 
@@ -11,43 +9,60 @@ VIDEO_MODE = 'fd_set_capture_mode&data=0'
 RECORD = 'fd_record'
 
 def take_picture():
-  print 'taking picture'
-  run(PHOTO_MODE)
-  run(RECORD)
+    print 'taking picture'
+    run(PHOTO_MODE)
+    run(RECORD)
 
 def start_recording():
-  print 'start recording'
-  run(VIDEO_MODE)
-  run(RECORD)
+    print 'start recording'
+    run(VIDEO_MODE)
+    run(RECORD)
 
 def stop_recording():
-  print 'end recording'
-  run(RECORD)
+    print 'end recording'
+    run(RECORD)
 
 def get_latest_files(reference_file):
-  print 'getting file names'
-  camera_files = []
-  if reference_file is None:
-    entries = run(LIST_ALL_FILES)
-  else:
-    reference_file = str.split(reference_file, 'DCIM/')[1]
-    entries = run(LIST_NEW_FILES + reference_file)
+    print 'getting file names'
+    camera_files = []
+    
+    # this command has two forms
+    # you can either ask camera to return all files or just 
+    # files created after a specific file that you know exists on the camera
+    # so this if statements is figuring out which version you'd like to use
+    if reference_file is None:
+      entries = run(LIST_ALL_FILES)
+    else:
+      # filepath is relative to DCIM folder so that's what the camera wants
+      # but we return the whole path, so I'm stripping out everything before and including DCIM
+      reference_file = str.split(reference_file, 'DCIM/')[1]
+      entries = run(LIST_NEW_FILES + reference_file)
+    
+    # we an get a TypeError if there are no new files created
+    # or no files at all on camera. Is it a problem that I just pass?
+    try:
+        #camera returns all files wrapped in json objects, but the outside is some xml syntax
+        #so I couldn't just do json.load() so I decided to use some regex to just strip out 
+        # the relative filepath, leaving behind createtime and size
+        files = re.findall("(100DRIFT\S\w+\W\w+)", entries)
+        for file in files:
+            file = 'http://{0}/DCIM/{1}'.format(IP, file) 
+            camera_files.append(file)
+    except TypeError:
+       pass
+  
 
-  try:
-    files = re.findall("(100DRIFT\S\w+\W\w+)", entries)
-    for file in files:
-      file = 'http://{0}/DCIM/{1}'.format(IP, file) 
-      camera_files.append(file)
-  except TypeError:
-     pass
- 
-  return camera_files 
+    return camera_files 
     
 def run(request):
-  try:
-    base_url = 'http://{0}/setting/cgi-bin/fd_control_client?func='.format(IP)
-    r = requests.get(base_url + request)
-    return r.text
-  except requests.exceptions.ConnectionError, e:
-    print 'restoring wifi connection with camera'
-    print e
+    # decided to have just one function that executes the requests call
+    # we catch the one type of error I've seen the camera do
+    # maybe this is a good place to then try to bring hostapd back up or reboot camera
+    # but will need work on hardware side some soldering of battery stuff and I'm not sure
+    try:
+        base_url = 'http://{0}/setting/cgi-bin/fd_control_client?func='.format(IP)
+        r = requests.get(base_url + request)
+        return r.text
+    except requests.exceptions.ConnectionError, e:
+        print 'restoring wifi connection with camera'
+        print e
